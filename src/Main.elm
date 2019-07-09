143cd65bd7c0775a3fda1040d413b34a52a4809d
hahnah <superhahnah@gmail.com>
2019-07-09 20:42:18 +0900

    Implement picking nth next color in hue circle

--- src/Main.elm
+++ src/Main.elm
@@ -4,8 +4,9 @@ import Browser
 import Color exposing (Color)
 import Color.Convert exposing (colorToHex)
 import Html exposing (Html, div, input, text)
-import Html.Attributes exposing (type_, value)
+import Html.Attributes exposing (style, type_, value)
 import Html.Events exposing (onInput)
+import Parser exposing ((|.), (|=), Parser, float, spaces, succeed, symbol)
 
 
 main =
@@ -62,7 +63,10 @@ view model =
         colorHex =
             Color.Convert.colorToHex model.color
     in
-    div []
+    div
+        [ style "color" "#ffffff"
+        , style "background-color" "#123456"
+        ]
         [ input
             [ type_ "color"
             , value colorHex
@@ -71,3 +75,61 @@ view model =
             []
         , text colorHex
         ]
+
+{- progressing
+pickDyad : Color -> List Color
+pickDyad baseColor =
+    let
+        baseColorHsl =
+            baseColor
+                |> Color.Convert.colorToCssHsl
+                |> Parser.run hsl
+    in
+    case baseColorHsl of
+        Ok colorHsl ->
+            baseColor :: [ Color.hsl (abs (1 - colorHsl.h)) colorHsl.s colorHsl.l ]
+
+        Err _ ->
+            [ baseColor ]
+-}
+
+type alias Hsl =
+    { h : Float
+    , s : Float
+    , l : Float
+    }
+
+
+hsl : Parser Hsl
+hsl =
+    succeed Hsl
+        |. symbol "hsl("
+        |. spaces
+        |= float
+        |. symbol ","
+        |. spaces
+        |= float
+        |. symbol ","
+        |. spaces
+        |= float
+        |. symbol ")"
+
+pickNthNext : Color -> Int -> Int -> Result String Color
+pickNthNext baseColor n total =
+    let
+        hueDifferenceUnit = 1.0 / (toFloat total)
+        baseColorHsl =
+            baseColor
+                |> Color.Convert.colorToCssHsl
+                |> Parser.run hsl
+    in
+        case baseColorHsl of
+                Ok colorHsl ->
+                    let
+                        gainedHue = colorHsl.h + (toFloat n) * hueDifferenceUnit
+                        pickedHue = gainedHue - (toFloat (floor gainedHue))
+                    in
+                        Ok <| Color.hsl pickedHue colorHsl.s colorHsl.l
+
+                Err _ ->
+                    Err "Failed pickNthNext"
