124f01d06cf4dc1843f6d6c9fadacd83cfd410ba
hahnah <superhahnah@gmail.com>
2019-07-13 19:20:32 +0900

    Layout roughly

--- src/Main.elm
+++ src/Main.elm
@@ -3,12 +3,13 @@ module Main exposing (main)
 import Browser
 import Color exposing (Color)
 import Color.Convert exposing (colorToCssHsl, colorToCssRgb, colorToHex)
-import Element exposing (Element, column, el, html, htmlAttribute, layout, row, text)
+import Element exposing (Element, column, el, html, htmlAttribute, layout, row, text, width, height, fill, px, centerX, spacing)
 import Element.Background as Background
-import Html exposing (Html, input)
-import Html.Attributes exposing (height, style, type_, value, width)
-import Html.Events exposing (onInput)
+import Html exposing (Html)
+import Html.Attributes as Attributes
+import Html.Events as Events
 import Parser exposing ((|.), (|=), Parser, float, spaces, succeed, symbol)
+import Element.Border as Border
 
 
 main =
@@ -66,30 +67,75 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    let
-        colorHex =
-            Color.Convert.colorToHex model.color
-
-        dyad =
-            pickDyad model.color
-    in
     layout
         []
         (column
-            []
-            [ html
-                (input
-                    [ type_ "color"
-                    , value colorHex
-                    , onInput OnChange
+            [ width fill ]
+            [ viewHeader
+            , row
+                [ width fill
+                , height fill
+                ]
+                [ viewLeftPane model
+                , viewMainPane model
+                ]
+            ]
+        )
+        
+
+viewHeader : Element msg
+viewHeader =
+    row
+        [ width fill
+        , spacing 10
+        , Border.width 1
+        ]
+        [ el [ centerX ] <| text "Logo"
+        , el [ centerX ] <| text "ColorStew"
+        ]
+
+
+viewLeftPane : Model -> Element Msg
+viewLeftPane model =
+    column
+        [ width <| px 350
+        , height fill
+        , Border.width 1
+        ]
+        [ row
+            [ width fill
+            , spacing 10
+            , Border.width 1
+            ]
+            [ text "PickMainColor"
+            , html
+                (Html.input
+                    [ Attributes.type_ "color"
+                    , Attributes.value <| Color.Convert.colorToHex model.color
+                    , Events.onInput OnChange
                     ]
                     []
                 )
-            , text colorHex
-            , viewColorSet dyad
             ]
-        )
+        , row
+            [ width fill
+            , spacing 10
+            , Border.width 1
+            ]
+            [ text "ColorShemes"
+            , text "Filter"
+            ]
+        , viewColorScheme "Dyad" <| pickDyad model.color
+        ]
 
+
+viewColorScheme : String -> List Color -> Element msg
+viewColorScheme scheme colors =
+    column
+        [ spacing 10 ]
+        [ text scheme
+        , viewColorSet colors
+        ]
 
 viewColorSet : List Color -> Element msg
 viewColorSet colors =
@@ -104,6 +150,44 @@ viewColor color =
     el
         [ Background.color color ]
         (text "\u{3000}\u{3000}\u{3000}\u{3000}")
+
+
+viewMainPane : Model -> Element msg
+viewMainPane model =
+    let
+        stewedColor : Color
+        stewedColor = Color.darkPurple -- provisional
+    in
+    column
+        [ width fill
+        , Border.width 1
+        ]
+        [ el [ centerX ] <| text "PREVIEW AREA"
+        , row
+            [ width fill ]
+            [ viewStewedColor stewedColor 0 0 
+            , viewStewedColor stewedColor 0 0
+            ]
+        ]
+
+
+viewStewedColor : Color -> Float -> Float -> Element msg
+viewStewedColor color saturation lightness =
+    column
+       [ width fill
+       , Border.width 1
+       ]
+       [ row
+           [ centerX
+           , spacing 10
+           ]
+           [ text "#Hex"
+           , text "Copy"
+           ]
+       , el [ centerX ] <| text "Color"
+       , el [ centerX ] <| text "SaturationSlider"
+       , el [ centerX ] <| text "LightnessSlider"
+       ]
 
 
 pickDyad : Color -> List Color
